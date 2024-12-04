7670c7615814ec255e0c118a181c1626eddab58f
Adil Zhexenov <tekmyktyadamdar@gmail.com>
2024-12-04 21:56:31 +0500

    lesson 10: create and remove elements in dom

--- js/script.js
+++ js/script.js
@@ -1,16 +1,47 @@
-// 1st task
-document.title = "Мой новый заголовок!";
+// 1.a
+const newSection = document.createElement("section");
+newSection.className = "highlight";
 
-// 2nd task
-const divBlock = document.getElementById("main");
-console.log(divBlock);
+// 1.b 
+const newSecondHeading = document.createElement("h2");
+newSecondHeading.textContent = "Динамический заголовок";
 
-const firstParagraph = document.querySelector(".text");
-console.log(firstParagraph);
+const newOrderedList = document.createElement("ol");
+const items = ["Первый пункт", "Второй пункт", "Третий пункт"];
 
-const allItems = document.querySelectorAll("li");
-console.log(allItems);
-
-for (let i = 0; i < allItems.length; i++) {
-  console.log(allItems[i].textContent);
+for (const item of items) {
+  const newListItem = document.createElement("li"); // <li></li>
+  newListItem.textContent = item; // 1. <li>Первый пункт</li> 2. <li>Второй пункт</li> 3. <li>Третий пункт</li>
+  newOrderedList.append(newListItem);
 }
+
+newSection.append(newSecondHeading, newOrderedList);
+
+// 1.c
+const container = document.querySelector("#container");
+container.append(newSection);
+
+// 2.a
+const myLink = document.getElementById("myLink");
+// 2.b
+myLink.textContent = "Перейти на Google";
+// 2.c
+myLink.href = "https://google.com";
+// 2.d
+myLink.setAttribute("target", "_blank");
+
+
+// 3.a
+const newFirstListItem = document.createElement("li");
+newFirstListItem.textContent = "Новый первый пункт";
+document.getElementById("myList").prepend(newFirstListItem);
+// 3.b
+const newLastListItem = document.createElement("li");
+newLastListItem.textContent = "Новый последний пункт";
+document.querySelector("#myList").append(newLastListItem);
+
+
+// 4
+const removableContainer = document.querySelector("#removableContainer");
+const removableItems = removableContainer.querySelectorAll("p");
+removableContainer.removeChild(removableItems[1]);
